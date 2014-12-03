3ef591740a3101c091d93a32ff9477e7242b4873
andrewleu <root@esx.(none)>
2014-12-03 13:32:59 +0800

    update sohu.tcl

--- sohu.tcl
+++ sohu.tcl
@@ -1,6 +1,5 @@
 package require mysqltcl
 global mysqlstatus
-
 set port {3306}
 set host {127.0.0.1}
 set user {ipv6bgp}
@@ -11,11 +10,9 @@ set unzipfile /home/rsync/shtemp
 set insertfile /var/lib/mysql/data/shtemp1
 cd /home/rsync/files
 set flist [lsort [glob sohu*]]
-
 set init_time [clock seconds]
 set init_time [clock format $init_time -format  {%Y-%m-%d %H:%M:%S}]
 # start time
-
 set mysql_handler [mysqlconnect -host $host -port $port -user $user -password $password]
 set infobase [ mysqlconnect -host $host -port $port -user $user -password $password]
 mysqluse $infobase $ipinfo
@@ -24,7 +21,6 @@ set lastfile [mysqlsel $mysql_handler "select id, filename from webfiles where i
 set lastfile [lindex $lastfile 0 1]
 set n_file [lsearch $flist $lastfile]
 incr n_file
-
 #set files [glob sohu*]
 while { [set read_file [lindex $flist $n_file]]!=""} {
    exec gzip -dc $read_file > $unzipfile
@@ -34,71 +30,37 @@ while { [set read_file [lindex $flist $n_file]]!=""} {
    #fconfigure $fp -encoding gb2312
    gets $fp msg
    set line 1
-  
             # next line edition
-
    while {$msg != ""} {
             while { [string index $msg 0] != "\u641c" && $msg != "" } {
-            gets $fp msg } ; # find the starting line
-            if { $msg != ""}  {  
-  if 0 {
- # ......
-# /* following line is for the uplevel location searching
-                set num 0
-                set count 0
-                set chrct [string index $msg $num]
-                set field_begin 0
-                set field_end 0
-                while {$chrct !=  ""} {
-                     if {$chrct ==","} {
-                             incr count
-                          if {$count == 2}  {
-                               set field_begin $num
-                          
-                           }   elseif { $count ==3 }    {
-                               set field_end $num
-                               break
-                         } 
-                        }   
-                        incr num    
-                        set chrct [string index $msg $num]
-                     }     ; # abstracting the location name
-           set location [    string rang $msg [expr $field_begin+1] [expr $field_end-1]]
-           set end_word [string index $location end ]
-           set location [string replace $location end end "%"] ; # for searching, use wild card
-           set locid [mysqlsel $infobase  "select id, pid from region where name like '$location' " -list]
-           if { [lindex $locid 0 1] !=0} { ; #the location name is at first level? no 
-                set uplevel [mysqlsel $infobase "select id, pid, name from region where id = '[lindex $locid 0 1]'" -list]
-                set parent [lindex $uplevel 0 2]
-                set msg [string replace $msg $field_begin $field_begin ",$parent,"] ; #insert the parent field
-                set field_end [expr $field_end+[  string length $parent]+1] ; # increase the end pointer include the coma byte
-                if { [lindex $uplevel 0 1] !=0 } { ; # the location name is at third level? yes
-                           set uplevel [mysqlsel $infobase "select id, pid, name from region where id = '[lindex $uplevel 0 1]'" -list ]
-                           set parent [lindex $uplevel 0 2]
-                           set msg [string replace $msg $field_begin $field_begin ",$parent,"]
-                           set field_end [expr $field_end+[  string length $parent]]
-                } else { ; # the location name is at second level, filling the third field
-                           set msg [string replace $msg $field_end $field_end ",-,"]
-                     }
-           } else  { ; #location name is at first level
-                 set msg [ string replace $msg $field_end $field_end ",-,-,"]
-           }  
-# end of uplevel searching */
-} 
+            gets $fp msg
+            set msg [string map {"\"" ""} $msg]
+            } ; # find the starting line
+            if { $msg != ""}  {
+              set partition_col [string map {"," " "} $msg]
+              set partition_col [lindex $partition_col 6]
+              set hours [string range $partition_col 8 9]
+              set partition_col [string rang $partition_col 0 5]
+              if { $hours >=19 && $hours < 23 }  {
+                     set status "b"
+              } elseif {
+                 $hours >= 1 && $hours <6
+              }  {
+                     set status "i"
+              } else {set status "o"}
+           set msg $msg,$partition_col,$status
            puts $tf $msg
-           gets $fp msg
            incr line
-           puts $line
-       }   
+           gets $fp msg
+           set msg [string map {"\"" ""} $msg]
+       }
       }
        close $fp
        close $tf
        mysqlexec $mysql_handler "load data  infile '$insertfile'into table webbrowsing Fields Terminated By ',' ( name,     ip,province,
-        city,county,carrier,date, os,tcp,terminal,browser,url,1stscreen,1stpage,pagesize,domready)"
+        city,county,carrier,date, os,tcp,terminal,browser,url,1stscreen,1stpage,pagesize,domready,prtcol,status)"
        mysqlexec $mysql_handler "insert into webfiles (filename, starttime, endtime) values ('$read_file','$init_time', now())"
-       incr n_file
-}     
-  
+       incr n_file;puts line
+}
 mysqlclose $mysql_handler
-mysqlclose $infobase   
-
+mysqlclose $infobase
